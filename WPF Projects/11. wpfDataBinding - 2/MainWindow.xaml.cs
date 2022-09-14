9e3bf09660f127266f8ee28526177567dc44a784
juhyung1021 <juhyung1021@gmail.com>
2022-09-14 14:07:04 +0900

    Add WPF 11 Project

---
+++ WPF Projects/11. wpfDataBinding - 2/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11.wpfDataBinding___2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Binding bind = new Binding();
+            bind.Source = text;
+            bind.Path = new PropertyPath(TextBox.TextProperty);
+
+            label.SetBinding(Label.ContentProperty, bind);
+        }
+    }
+}
