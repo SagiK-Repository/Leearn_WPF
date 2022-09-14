833fa56d8668da9356ab36ab7875c7767d7d42d6
juhyung1021 <juhyung1021@gmail.com>
2022-09-14 11:16:16 +0900

    Add WPF 09 Project

---
+++ WPF Projects/09. wpfRoutedEvent - 2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _09.wpfRoutedEvent___2
+{
+    public partial class MainWindow : Window
+    {
+        private string _mouseActivity = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _mouseActivity = "PreviewMouseDown Window \n";
+            MessageBox.Show(_mouseActivity);
+        }
+
+        private void StackPanel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _mouseActivity = "PreviewMouseDown StackPanel \n";
+            MessageBox.Show(_mouseActivity);
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _mouseActivity = "PreviewMouseDown Button \n";
+            MessageBox.Show(_mouseActivity);
+        }
+
+    }
+}
---
+++ WPF Projects/09. wpfRoutedEvent/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace _09.wpfRoutedEvent
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = "Button is Clicked";
+            e.Handled = false; // 버블링 이벤트 제거 : true
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            txt2.Text = "Click event is bubbled to Stack Panel";
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            txt3.Text = "Click event is bubbled to Window";
+        }
+
+    }
+}
